9adeb02673d30919cf1d663327895237d1de549c
Preil <preil2000@gmail.com>
2020-09-11 08:48:54 +0300

    Function to short the text to max length.

--- components/sidebar.js
+++ components/sidebar.js
@@ -4,23 +4,6 @@ import { useState } from 'react'
 
 const Sidebar = () => {
 
-  const [count, setCount] = useState(0);
-  // it is the same as:
-  // const stateArray = useState(0);
-  // const count = stateArray[0];
-  // const setCount = stateArray[1];
-
-
-  const increment = ()=>{
-    const newCount = count + 1;
-     setCount(newCount)
-  }
-
-  const decrement = ()=>{
-    const newCount = count - 1;
-    setCount(newCount)
-  }
-
 
   return (
     <>
@@ -30,13 +13,6 @@ const Sidebar = () => {
         <a href="#" className="list-group-item">Category 2</a>
         <a href="#" className="list-group-item">Category 3</a>
       </div>
-
-      <div>
-        <button onClick={increment} className="btn btn-primary">Increment</button>
-        <button onClick={decrement} className="btn btn-primary">Decrement</button>
-        <h1>{count}</h1>
-      </div>
-
     </>
   )
 }
