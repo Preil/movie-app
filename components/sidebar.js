6505cfd7947487ef8a01be37f146e8354ac52165
Preil <preil2000@gmail.com>
2020-09-15 17:15:00 +0300

    Create movie modal started.

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-
+import Modal from '../components/modal'
 
 
 const Sidebar = (props) => {
   const {categories} = props
   return (
     <>
+      <Modal/>
       <h1 className="my-4">Shop Name</h1>
       <div className="list-group">
         {categories.map((category)=>{
