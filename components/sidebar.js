bf6217829f0645154bad1091f36a6f9d20e8789d
Preil <preil2000@gmail.com>
2020-09-15 17:29:00 +0300

    Create movie form added as containment.

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Modal from '../components/modal'
+import MovieCreateForm from './movieCreateForm'
 
 
+// Containment example
 const Sidebar = (props) => {
   const {categories} = props
   return (
     <>
-      <Modal/>
+      <Modal>
+        <MovieCreateForm/>
+      </Modal>
       <h1 className="my-4">Shop Name</h1>
       <div className="list-group">
         {categories.map((category)=>{
