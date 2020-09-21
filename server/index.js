46aab7cc21eccf094e7a27b8fbc346614f31ae65
Preil <preil2000@gmail.com>
2020-09-21 09:50:41 +0300

    Parsing data from POST request.

--- server/index.js
+++ server/index.js
@@ -1,39 +1,46 @@
-
 const next = require('next');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app=next({dev})
+const app = next({dev})
 const handle = app.getRequestHandler()
 
-app.prepare().then(()=>{
+app.prepare().then(() => {
   const server = express();
+  server.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  server.use(bodyParser.json());
 
-  server.get('/api/v1/movies', (req, res)=>{
+
+  server.get('/api/v1/movies', (req, res) => {
     return res.json({message: 'hello world'})
   })
 
-  server.post('/api/v1/movies', (req, res)=>{
-    return res.json({message: 'Saving Post'})
+  server.post('/api/v1/movies', (req, res) => {
+    const movie = req.body;
+    console.log(JSON.stringify(movie));
+    return res.json({...movie, createdTime: 'today', author: 'ILYA'})
   })
 
-  server.patch('/api/v1/movies/:id', (req, res)=>{
+  server.patch('/api/v1/movies/:id', (req, res) => {
     const {id} = req.params;
     return res.json({message: `Updating post of id: ${id}`})
   })
 
-  server.delete('/api/v1/movies/:id', (req, res)=>{
+  server.delete('/api/v1/movies/:id', (req, res) => {
     const {id} = req.params;
     return res.json({message: `Deleting post of id: ${id}`})
   })
 
-  server.get('*',(req, res)=>{
+  server.get('*', (req, res) => {
     return handle(req, res)
   })
 
   const PORT = process.env.PORT || 3000;
-  server.listen(PORT,(err)=>{
-    if(err) throw err
+  server.listen(PORT, (err) => {
+    if (err) throw err
     console.log('> Ready on port ' + PORT)
   })
 })
