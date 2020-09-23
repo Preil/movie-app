19789d4cab0de921e2c49449051ce35c516c9bf1
Preil <preil2000@gmail.com>
2020-09-23 11:29:20 +0300

    Getting movie from server by id. Saving new movie to data.json

--- actions/index.js
+++ actions/index.js
@@ -11,9 +11,9 @@ const CATEGORY_DATA = [
   {id: 4, name: 'historical'}
 ]
 
-export const getCategories = () =>{
-  return new Promise((resolve, reject)=>{
-    setTimeout(()=>{
+export const getCategories = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
       resolve(CATEGORY_DATA)
     }, 100)
   })
@@ -27,30 +27,17 @@ export const getMovies = () => {
   //   }, 100)
   // })
 
-  return axios.get(`${BASE_URL}/api/v1/movies`).then((res)=>{
-    console.log(res);
+  return axios.get(`${BASE_URL}/api/v1/movies`).then((res) => {
     return res.data
   })
 };
 
 export const createMovie = (movie) => {
-
-  return new Promise((resolve, reject) => {
-    movie.id = Math.random().toString(36).substr(2, 7);
-    MOVIE_DATA.push(movie);
-    setTimeout(() => {
-      resolve(MOVIE_DATA)
-    }, 100)
-  })
+  movie.id = Math.random().toString(36).substr(2, 5);
+  return axios.post(`${BASE_URL}/api/v1/movies`, movie).then(res => res.data)
 };
 
 
 export const getMovieById = (id) => {
-  return new Promise((resolve, reject) => {
-    const movieIndex = MOVIE_DATA.findIndex((movie) => {
-      return movie.id === id
-    })
-    const movie = MOVIE_DATA[movieIndex]
-    setTimeout(() => resolve(movie), 50)
-  })
+  return axios.get(`${BASE_URL}/api/v1/movies/${id}`).then(res => res.data);
 }
--- components/sidebar.js
+++ components/sidebar.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import {useState} from 'react'
+import {useRouter} from 'next/router'
 import Modal from '../components/modal'
 import MovieCreateForm from './movieCreateForm'
 import {createMovie} from "../actions";
@@ -6,11 +7,12 @@ import {createMovie} from "../actions";
 // Containment example
 const Sidebar = (props) => {
   const {categories} = props
+  const router = useRouter()
   let modal = null
-  const handleCreateMovie = (movie) =>{
-    createMovie(movie).then((movies)=>{
-      console.log(JSON.stringify(movies))
+  const handleCreateMovie = (movie) => {
+    createMovie(movie).then((movies) => {
       modal.closeModal()
+      router.push('/')
     })
   }
 
@@ -22,12 +24,12 @@ const Sidebar = (props) => {
       </Modal>
       <h1 className="my-4">Shop Name</h1>
       <div className="list-group">
-        {categories.map((category)=>{
+        {categories.map((category) => {
           return (
             <a key={category.id} href="#" className="list-group-item">{category.name}</a>
           )
         })}
-        </div>
+      </div>
     </>
   )
 }
